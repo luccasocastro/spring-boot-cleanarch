909a38bb96b742c710be43ddf26c0377e09765b8
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-22 11:01:34 -0300

    feat: converting req n' res to entity

---
+++ src/main/java/com/luxkapotter/springbootcleanarch/infrastructure/controllers/UserDTOMapper.java
@@ -0,0 +1,13 @@
+package com.luxkapotter.springbootcleanarch.infrastructure.controllers;
+
+import com.luxkapotter.springbootcleanarch.domain.entity.User;
+
+public class UserDTOMapper {
+    CreateUserResponse toResponse(User user){
+        return new CreateUserResponse(user.username(), user.email());
+    }
+
+    public User toUser(CreateUserRequest request){
+        return new User(request.username(), request.password(), request.email());
+    }
+}
