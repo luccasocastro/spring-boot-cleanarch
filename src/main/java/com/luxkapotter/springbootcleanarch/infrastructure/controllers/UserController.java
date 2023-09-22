cf63dbc111c1211ce17c9ff17c8dec95bee66572
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-22 10:59:26 -0300

    feat: creating a new controller with the new rules

---
+++ src/main/java/com/luxkapotter/springbootcleanarch/infrastructure/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.luxkapotter.springbootcleanarch.infrastructure.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luxkapotter.springbootcleanarch.application.usecases.CreateUserInteractor;
+import com.luxkapotter.springbootcleanarch.domain.entity.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    private final CreateUserInteractor createUserInteractor;
+    private final UserDTOMapper userDTOMapper;
+
+    UserController(CreateUserInteractor createUserInteractor, UserDTOMapper userDTOMapper) {
+        this.createUserInteractor = createUserInteractor;
+        this.userDTOMapper = userDTOMapper;
+    }
+
+    @PostMapping
+    CreateUserResponse create(@RequestBody CreateUserRequest request){
+        User userBusinessObj = userDTOMapper.toUser(request);
+        User user = createUserInteractor.createUser(userBusinessObj);
+        return userDTOMapper.toResponse(user);
+    }
+
+}
